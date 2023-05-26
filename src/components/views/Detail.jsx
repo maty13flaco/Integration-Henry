4473168388151caaaaac2469a80d63814c53e824
Matias Munyo <munyomatias@hotmail.com>
2023-05-26 07:59:52 +0000

    Routin full integration

--- src/components/views/Detail.jsx
+++ src/components/views/Detail.jsx
@@ -1,8 +1,36 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
 const Detail = () => {
-  return (
+  const id = useParams().id;
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+      ({ data }) => {
+        if (data.name) {
+          setCharacter(data);
+        } else {
+          window.alert("No hay personajes con ese ID");
+        }
+      }
+    );
+    return setCharacter({});
+  }, [id]);
+
+  return character ? (
     <div>
-      <h1>Detail view</h1>
+      <h1>{character.name}</h1>
+      <span>Status: {character.status}</span>
+      <span>Specie: {character.species}</span>
+      <span>Gender: {character.gender}</span>
+      <span>Origin: {character.origin?.name}</span>
+      <img src={character.image} />
     </div>
+  ) : (
+    ""
   );
 };
 
