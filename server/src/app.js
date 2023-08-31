abe4b4b1c953807e77d8dcee3f463d7acaa5383c
maty13.flaco <munyomatias@hotmail.com>
2023-08-31 20:46:19 -0300

    db in progress

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,28 @@ const express = require("express");
 const data = require("./utils/data");
 const routes = require("./routes/index")
 
+const PORT = 3001;
+
 const app = express();
 
-app.use((req,res,next) =>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  })
-  app.use(express.json())
-  app.use('/rickandmorty', routes)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+app.use(express.json());
+app.use("/rickandmorty", routes);
 
-  module.exports = app
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+  
+  // module.exports = app
